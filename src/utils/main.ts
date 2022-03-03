2ea4082bcf01a2fd86d2a4804fc6459fee6e2b14
Ptolemy Powlesland <ptolemypowlesland@gmail.com>
2022-03-03 01:38:35 +0000

    Implement sudoku solver function

---
+++ src/utils/main.ts
@@ -0,0 +1,72 @@
+type Board = (number | null)[][];
+
+export function solveBoard(board: Board): Board | false {
+  const nextCell = findNextCell(board);
+  if (!nextCell) return board;
+  for (let i = 1; i <= 9; i += 1) {
+    const [row, col] = nextCell;
+    if (placeDigit(i, row, col, board)) {
+      const current = solveBoard(board);
+      if (current) return board;
+    }
+  }
+  return false;
+}
+
+function findNextCell(board: Board) {
+  for (let i = 0; i < 9; i += 1) {
+    for (let j = 0; j < 9; j += 1) {
+      if (board[i][j] === null) return [i, j];
+    }
+  }
+  return false;
+}
+
+function isCellValid(row: number, col: number, board: Board) {
+  if (!checkRowOrCol(row, col, board, "horizontal")) return false;
+  if (!checkRowOrCol(row, col, board, "vertical")) return false;
+  if (!checkSubgrid(row, col, board)) return false;
+  return true;
+}
+
+function checkSubgrid(row: number, col: number, board: Board) {
+  const values = new Set();
+  const startRow = Math.floor(row / 3) * 3;
+  const startCol = Math.floor(col / 3) * 3;
+  for (let i = startRow; i < startRow + 3; i += 1) {
+    for (let j = startCol; j < startCol + 3; j += 1) {
+      const currentVal = board[i][j];
+      if (currentVal !== null) {
+        if (!values.has(currentVal)) values.add(currentVal);
+        else return false;
+      }
+    }
+  }
+  return true;
+}
+
+function checkRowOrCol(
+  row: number,
+  col: number,
+  board: Board,
+  direction: string
+) {
+  const values = new Set();
+  for (let i = 0; i < 9; i += 1) {
+    let currentVal;
+    if (direction === "horizontal") currentVal = board[row][i];
+    if (direction === "vertical") currentVal = board[i][col];
+    if (currentVal !== null) {
+      if (!values.has(currentVal)) values.add(currentVal);
+      else return false;
+    }
+  }
+  return true;
+}
+
+function placeDigit(value: number, row: number, col: number, board: Board) {
+  board[row][col] = value;
+  if (isCellValid(row, col, board)) return true;
+  board[row][col] = null;
+  return false;
+}
