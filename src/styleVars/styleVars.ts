615e721aa2f98df56a848f4814f4c66c15baf51b
Ptolemy Powlesland <ptolemypowlesland@gmail.com>
2022-03-25 17:33:50 +0000

    Change seconday color

--- src/styleVars/styleVars.ts
+++ src/styleVars/styleVars.ts
@@ -15,11 +15,12 @@ export const font_size = "30px";
 export const font_family = "Roboto";
 export const background_color = "26, 51, 63";
 export const background_alt_color = "16, 32, 39";
-export const primary_color = "54, 224, 173";
+export const primary_color = "54, 224, 173"; //54, 224, 173
+// export const primary_color = "84, 158, 255";
 export const primary_color_alpha = "0.4";
-// export const secondary_color = "223, 52, 55";
+export const secondary_color = "84, 158, 255";
 // export const secondary_color = "247, 179, 92";
-export const secondary_color = "255, 198, 125";
+// export const secondary_color = "255, 198, 125";
 // export const font_family =
 //   '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;';
 export const padding = 20;
